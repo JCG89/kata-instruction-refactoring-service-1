459418273f22dc0507e35ce3e2be8f581031ceb6
Jean Gomis <jeangomistech2018@gmail.com>
2024-05-16 18:01:47 +0200

    creation of the CompagnConfig class to make the urls dynamic

--- excelservice-1.0/src/main/java/com/newlight77/kata/survey/config/CompaignConfig.java
+++ excelservice-1.0/src/main/java/com/newlight77/kata/survey/config/CompaignConfig.java
@@ -1,2 +1,16 @@
-package com.newlight77.kata.survey.config;public class CompaignConfig {
+package com.newlight77.kata.survey.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "external")
+public class CompaignConfig {
+    private String url;
+    private EndPointConfiguration endpoint;
+
 }
