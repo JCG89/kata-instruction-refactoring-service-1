810c1862c36a1c8f864d4a9ffe05bb7bcb54c804
Jean Gomis <jeangomistech2018@gmail.com>
2024-05-16 18:10:51 +0200

    Changing the calling of uris in CampaignClient with the CampaignConfig type

--- excelservice-1.0/src/main/java/com/newlight77/kata/survey/client/CampaignClient.java
+++ excelservice-1.0/src/main/java/com/newlight77/kata/survey/client/CampaignClient.java
@@ -1,47 +1,56 @@
 package com.newlight77.kata.survey.client;
 
+import com.newlight77.kata.survey.config.CompaignConfig;
 import com.newlight77.kata.survey.model.Campaign;
 import com.newlight77.kata.survey.model.Survey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Service
+@Component
 public class CampaignClient {
+    /*
+    L'annotation @Service est exclusivement reservée  aux class qui incluent du code business
+    Un code business est un code demandé par le Poo
+     */
 
     private final WebClient webClient;
+    private final CompaignConfig campaignConfig;
 
-    public CampaignClient(@Value("${external.url}") final String externalUrl) {
+    public CampaignClient(CompaignConfig campaignConfig) {
+        this.campaignConfig = campaignConfig;
         webClient = WebClient.builder()
-                .baseUrl(externalUrl)
+                .baseUrl(campaignConfig.getUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
                 .build();
     }
 
     public void createSurvey(final Survey survey) {
         webClient.post()
-                .uri("/surveys")
+                .uri(campaignConfig.getEndpoint().getSurvey())
                 .bodyValue(survey)
                 .retrieve();
     }
 
     public Survey getSurvey(final String id) {
         return webClient.get()
-                .uri("/surveys/" + id)
+                .uri(campaignConfig.getEndpoint().getSurvey() + "/" + id)
                 .retrieve()
                 .bodyToMono(Survey.class).block();
     }
 
     public void createCampaign(final Campaign campaign) {
         webClient.post()
-                .uri("/campaigns")
+                .uri(campaignConfig.getEndpoint().getCampaign())
                 .bodyValue(campaign);
     }
 
     public Campaign getCampaign(final String id) {
         return webClient.get()
-                .uri("/campaigns/" + id)
+                .uri(campaignConfig.getEndpoint().getCampaign() +"/" + id)
                 .retrieve()
                 .bodyToMono(Campaign.class).block();
     }
