01df63fde9287ccf7b371e855ba6868cf4ed400d
Jean Gomis <jeangomistech2018@gmail.com>
2024-05-16 21:46:20 +0200

    added @Setter annotation

--- excelservice-1.0/src/main/java/com/newlight77/kata/survey/config/EndPointConfiguration.java
+++ excelservice-1.0/src/main/java/com/newlight77/kata/survey/config/EndPointConfiguration.java
@@ -1,2 +1,16 @@
-package com.newlight77.kata.survey.config;public class EndPointConfiguration {
+package com.newlight77.kata.survey.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "endpoint")
+public class EndPointConfiguration {
+    private String survey;
+    private String campaign;
+
 }
